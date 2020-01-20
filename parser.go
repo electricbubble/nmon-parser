e713eae54954fb101566b9e011e924e9aa9c960f
ElectricBubble <bubble_yang@yeah.net>
2020-01-20 13:15:04 +0800

    修复空行导致的报错

--- parser.go
+++ parser.go
@@ -16,27 +16,28 @@ func ParseNmonByFilename(name string) (nmon Nmon, err error) {
 		_ = nmonFile.Close()
 	}()
 
-	nmon = newNmon()
-
-	reader := bufio.NewReader(nmonFile)
-	line, err := reader.ReadString('\n')
-	for err == nil {
-		indexSep := strings.Index(line, ",")
-		cls := line[:indexSep]
-		line = line[indexSep+1 : len(line)-1]
-		// fmt.Println(cls, line)
-		nmon.saveData(cls, line)
-		line, err = reader.ReadString('\n')
-	}
-	if err != io.EOF {
-		return Nmon{}, err
-	}
-
-	// fmt.Println(nmon.seriesClass)
-	nmon.sortSeriesClass()
-	// fmt.Println(nmon.seriesClass)
-
-	return nmon, nil
+	// nmon = newNmon()
+	//
+	// reader := bufio.NewReader(nmonFile)
+	// line, err := reader.ReadString('\n')
+	// for err == nil {
+	// 	indexSep := strings.Index(line, ",")
+	// 	cls := line[:indexSep]
+	// 	line = line[indexSep+1 : len(line)-1]
+	// 	// fmt.Println(cls, line)
+	// 	nmon.saveData(cls, line)
+	// 	line, err = reader.ReadString('\n')
+	// }
+	// if err != io.EOF {
+	// 	return Nmon{}, err
+	// }
+	//
+	// // fmt.Println(nmon.seriesClass)
+	// nmon.sortSeriesClass()
+	// // fmt.Println(nmon.seriesClass)
+	//
+	// return nmon, nil
+	return ParseNmonByReader(nmonFile)
 }
 
 func ParseNmonByReader(rd io.Reader) (nmon Nmon, err error) {
@@ -46,6 +47,10 @@ func ParseNmonByReader(rd io.Reader) (nmon Nmon, err error) {
 	line, err := reader.ReadString('\n')
 	for err == nil {
 		indexSep := strings.Index(line, ",")
+		if indexSep == -1 && len(line) == 1 {
+			// 空行
+			continue
+		}
 		cls := line[:indexSep]
 		line = line[indexSep+1 : len(line)-1]
 		// fmt.Println(cls, line)
